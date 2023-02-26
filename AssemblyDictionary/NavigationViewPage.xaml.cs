6c436bfcf90f7bda2c86f821e3ac166c587a95e7
Poker <poker_sang@outlook.com>
2023-02-26 13:54:15 +0800

    添加搜索、回退功能

--- AssemblyDictionary/NavigationViewPage.xaml.cs
+++ AssemblyDictionary/NavigationViewPage.xaml.cs
@@ -2,7 +2,9 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using Windows.System;
 using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Input;
 using Microsoft.UI.Xaml.Navigation;
 using WinUI3Utilities;
 
@@ -12,22 +14,56 @@ public sealed partial class NavigationViewPage : Page
 {
     public NavigationViewPage() => InitializeComponent();
 
-    protected override void OnNavigatedTo(NavigationEventArgs e) =>
+    protected override void OnNavigatedTo(NavigationEventArgs e)
+    {
         NavigationView.MenuItemsSource = Directory.GetFileSystemEntries(e.Parameter.To<string>())
             .Select(t => new FileItem(Path.GetFileNameWithoutExtension(t).Replace('_', '/').Replace('!', '?'), t));
+    }
 
-    private FileItem? _lastFileItem;
+    private FileItem? _currentSelection;
 
     private void ItemInvoked(NavigationView sender, NavigationViewItemInvokedEventArgs e)
     {
-        if (e.InvokedItem is FileItem fi && fi.Fullname != _lastFileItem?.Fullname)
-            _lastFileItem = fi;
-        else
+        if (e.InvokedItem is not FileItem fi)
             return;
-        if (Directory.Exists(_lastFileItem.Fullname))
-            _ = Frame.Navigate(typeof(TextBlockPage), _lastFileItem.Fullname);
-        else if (File.Exists(_lastFileItem.Fullname))
-            _ = Frame.Navigate(typeof(TextBlockPage), _lastFileItem.Fullname);
-        sender.Header = _lastFileItem.Name;
+        TryNavigate(fi);
+    }
+
+    private void OnTextChanged(AutoSuggestBox sender, AutoSuggestBoxTextChangedEventArgs e)
+    {
+        if (sender.Text is "")
+            return;
+        if (NavigationView.MenuItemsSource.To<IEnumerable<FileItem>>().FirstOrDefault(t => t.Name.StartsWith(sender.Text, StringComparison.OrdinalIgnoreCase)) is not { } fi)
+            return;
+        TryNavigate(fi);
+        // TODO: 无法自动切换Pane选项
+        NavigationView.SelectedItem = _currentSelection;
+    }
+    private void OnKeyUp(object sender, KeyRoutedEventArgs e)
+    {
+        if (!NavigationView.IsBackEnabled)
+            return;
+        // TODO: 接受不到事件
+        if (e.Key is VirtualKey.GoBack)
+            BackRequested(null!, null!);
+    }
+
+    private void BackRequested(NavigationView sender, NavigationViewBackRequestedEventArgs e)
+    {
+        Frame.GoBack();
+        _currentSelection = NavigationView.MenuItemsSource.To<IEnumerable<FileItem>>().First(t => t.Fullname == Frame.Content.To<TextBlockPage>().Parameter);
+        NavigationView.Header = _currentSelection.Name;
+    }
+
+    private void TryNavigate(FileItem item)
+    {
+        if (item.Fullname == _currentSelection?.Fullname)
+            return;
+        _currentSelection = item;
+        if (Directory.Exists(item.Fullname))
+            _ = Frame.Navigate(typeof(TextBlockPage), item.Fullname);
+        else if (File.Exists(path: item.Fullname))
+            _ = Frame.Navigate(typeof(TextBlockPage), item.Fullname);
+        NavigationView.Header = item.Name;
     }
 }
--- AssemblyDictionary/TextBlockPage.xaml.cs
+++ AssemblyDictionary/TextBlockPage.xaml.cs
@@ -9,7 +9,13 @@ public sealed partial class TextBlockPage : Page
 {
     public TextBlockPage() => InitializeComponent();
 
+    public string Parameter { get; private set; } = "";
+
     public TextBox TextBlockContent => Content.To<ScrollViewer>().Content.To<TextBox>();
 
-    protected override void OnNavigatedTo(NavigationEventArgs e) => TextBlockContent.Text = File.ReadAllText(e.Parameter.To<string>());
+    protected override void OnNavigatedTo(NavigationEventArgs e)
+    {
+        Parameter = e.Parameter.To<string>();
+        TextBlockContent.Text = File.ReadAllText(Parameter);
+    }
 }
