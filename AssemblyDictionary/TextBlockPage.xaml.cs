598ffd7427412595738e3676fb30b25a5931ba8e
Poker <poker_sang@outlook.com>
2023-02-26 15:28:54 +0800

    添加即时保存、重新加载文件功能

--- AssemblyDictionary/TextBlockPage.xaml.cs
+++ AssemblyDictionary/TextBlockPage.xaml.cs
@@ -1,5 +1,8 @@
+using System;
 using System.IO;
+using System.Threading.Tasks;
 using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Input;
 using Microsoft.UI.Xaml.Navigation;
 using WinUI3Utilities;
 
@@ -11,11 +14,40 @@ public sealed partial class TextBlockPage : Page
 
     public string Parameter { get; private set; } = "";
 
-    public TextBox TextBlockContent => Content.To<ScrollViewer>().Content.To<TextBox>();
-
     protected override void OnNavigatedTo(NavigationEventArgs e)
     {
         Parameter = e.Parameter.To<string>();
-        TextBlockContent.Text = File.ReadAllText(Parameter);
+        TextBox.Text = File.ReadAllText(Parameter);
     }
+
+    private void RefreshTapped(object sender, TappedRoutedEventArgs e)
+    {
+        TextBox.Text = File.ReadAllText(Parameter);
+        FadeOut("已从硬盘重新读取", Parameter);
+    }
+
+    private void SaveTapped(object sender, TappedRoutedEventArgs e)
+    {
+        File.WriteAllText(Parameter, TextBox.Text);
+        FadeOut("已保存到路径", Parameter);
+    }
+
+    #region SnackBar功能
+
+    private DateTime _closeSnakeBarTime;
+
+    private async void FadeOut(string title, string subtitle, int mSec = 3000)
+    {
+        _closeSnakeBarTime = DateTime.Now + TimeSpan.FromMicroseconds(mSec - 100);
+
+        SnackBar.Title = title;
+        SnackBar.Subtitle = subtitle;
+
+        _ = SnackBar.IsOpen = true;
+        await Task.Delay(mSec);
+        if (DateTime.Now > _closeSnakeBarTime)
+            _ = SnackBar.IsOpen = false;
+    }
+
+    #endregion
 }
