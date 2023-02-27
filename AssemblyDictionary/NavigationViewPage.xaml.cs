00b9c7199ea3d0bbebcd4773ea9da8847a7bce35
Poker <poker_sang@outlook.com>
2023-02-27 20:34:38 +0800

    快捷键

--- AssemblyDictionary/NavigationViewPage.xaml.cs
+++ AssemblyDictionary/NavigationViewPage.xaml.cs
@@ -2,17 +2,20 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using Windows.System;
 using Microsoft.UI.Xaml.Controls;
 using Microsoft.UI.Xaml.Input;
 using Microsoft.UI.Xaml.Navigation;
 using WinUI3Utilities;
+using Microsoft.UI.Xaml;
 
 namespace AssemblyDictionary;
 
 public sealed partial class NavigationViewPage : Page
 {
-    public NavigationViewPage() => InitializeComponent();
+    public NavigationViewPage()
+    {
+        InitializeComponent();
+    }
 
     protected override void OnNavigatedTo(NavigationEventArgs e)
     {
@@ -29,29 +32,23 @@ public sealed partial class NavigationViewPage : Page
         TryNavigate(fi);
     }
 
-    private void OnTextChanged(AutoSuggestBox sender, AutoSuggestBoxTextChangedEventArgs e)
+    private void TextChanged(AutoSuggestBox sender, AutoSuggestBoxTextChangedEventArgs e)
     {
         if (sender.Text is "")
             return;
-        if (NavigationView.MenuItemsSource.To<IEnumerable<FileItem>>().FirstOrDefault(t => t.Name.StartsWith(sender.Text, StringComparison.OrdinalIgnoreCase)) is not { } fi)
+        if (NavigationView.MenuItemsSource.To<IEnumerable<FileItem>>()
+                .FirstOrDefault(t => t.Name.StartsWith(sender.Text, StringComparison.OrdinalIgnoreCase)) is not { } fi)
             return;
         TryNavigate(fi);
         // TODO: 无法自动切换Pane选项
         NavigationView.SelectedItem = _currentSelection;
     }
-    private void OnKeyUp(object sender, KeyRoutedEventArgs e)
-    {
-        if (!NavigationView.IsBackEnabled)
-            return;
-        // TODO: 接受不到事件
-        if (e.Key is VirtualKey.GoBack)
-            BackRequested(null!, null!);
-    }
 
     private void BackRequested(NavigationView sender, NavigationViewBackRequestedEventArgs e)
     {
         Frame.GoBack();
-        _currentSelection = NavigationView.MenuItemsSource.To<IEnumerable<FileItem>>().First(t => t.Fullname == Frame.Content.To<TextBlockPage>().Parameter);
+        _currentSelection = NavigationView.MenuItemsSource.To<IEnumerable<FileItem>>()
+            .First(t => t.Fullname == Frame.Content.To<TextBlockPage>().Parameter);
         NavigationView.Header = _currentSelection.Name;
     }
 
@@ -62,8 +59,63 @@ public sealed partial class NavigationViewPage : Page
         _currentSelection = item;
         if (Directory.Exists(item.Fullname))
             _ = Frame.Navigate(typeof(TextBlockPage), item.Fullname);
-        else if (File.Exists(path: item.Fullname))
+        else if (File.Exists(item.Fullname))
             _ = Frame.Navigate(typeof(TextBlockPage), item.Fullname);
         NavigationView.Header = item.Name;
     }
+
+    #region 快捷键
+
+    private void PageUp(KeyboardAccelerator sender, KeyboardAcceleratorInvokedEventArgs e)
+    {
+        var items = NavigationView.MenuItemsSource.To<IEnumerable<FileItem>>().ToArray();
+        if (items.Length is 0)
+            return;
+        var last = items[^1];
+        foreach (var fi in items)
+            if (fi == NavigationView.SelectedItem.To<FileItem>())
+            {
+                NavigationView.SelectedItem = last;
+                TryNavigate(last);
+                return;
+            }
+            else
+                last = fi;
+    }
+
+    private void PageDown(KeyboardAccelerator sender, KeyboardAcceleratorInvokedEventArgs e)
+    {
+        var items = NavigationView.MenuItemsSource.To<IEnumerable<FileItem>>().ToArray();
+        if (items.Length is 0)
+            return;
+        var found = false;
+        foreach (var fi in items)
+            if (found)
+            {
+                NavigationView.SelectedItem = fi;
+                TryNavigate(fi);
+                return;
+            }
+            else if (fi == NavigationView.SelectedItem.To<FileItem>())
+                found = true;
+
+        NavigationView.SelectedItem = items[0];
+        TryNavigate(items[0]);
+    }
+
+    private void GoBack(object sender, PointerRoutedEventArgs e)
+    {
+        var currentPoint = e.GetCurrentPoint(sender.To<UIElement>());
+        if (currentPoint.PointerDeviceType is not Microsoft.UI.Input.PointerDeviceType.Mouse)
+            return;
+
+        if (currentPoint.Properties.IsXButton1Pressed)
+        {
+            if (!NavigationView.IsBackEnabled)
+                return;
+            BackRequested(null!, null!);
+        }
+    }
+
+    #endregion
 }
